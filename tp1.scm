46a025bc5bf3eef756c0cd797c7ff8c5d84a0615
Lilian Hiault <lilian.hiault@laposte.net>
2020-11-13 13:43:36 +0100

    Update tp1.scm

--- tp1.scm
+++ tp1.scm
@@ -5,7 +5,7 @@
 ;; Nouveau buffer
 ;; M-x run-scheme
 
-;; 0
+;; 0.
 
 (define carre
   (lambda ( x )
@@ -20,7 +20,7 @@
 
 (quad 5)
 
-;; 1
+;; 1.
 
 (define pi 3.14)
 
@@ -30,7 +30,7 @@
 	 (* pi (carre r)))))
 (cercle 3)
 
-;; 2
+;; 2.
 
 (define LA
   '(1
@@ -51,3 +51,80 @@
 ;; Ajouter une feuille à la racine 14
 
 (car (cddadr (cadddr LA))) ;; Sous arbre de racine 14
+
+;; 3.
+
+(define fact
+  (lambda (n)
+    (if (zero? n)
+	1
+	(* n (fact (- n 1)))
+	)))
+
+(define fac
+  (lambda (n)
+    (if (integer? n)
+	(fact n)
+	"n n'est pas un entier"
+	)))
+
+(fac 5)
+
+;; 4. Somme des n premiers entiers
+
+;; 5. (long L)
+
+(define long
+  (lambda (L)
+    (if (null? L)
+	0
+	(+ 1 (long (cdr L)))
+	)))
+
+(long '(1 2 3 5 3 1 0))
+
+;; 6.
+;; Ajouter x :
+;; () -> (list x)
+;; (e|L') -> (e|(ajoute x L)
+(define ajouter
+  (lambda (x L)
+    (if (null? L)
+	(list x)
+	(cons (car L) (ajouter x (cdr L)))
+	)))
+
+(ajouter 1 '(2 3 4 5 6))
+
+(trace ajouter)
+(ajouter 2 '(1 2 3))
+(untrace ajouter)
+
+;; Renverse L :
+;; () -> ()
+;; (car|cdr) -> (renverse cdr|car)
+(define renverse
+  (lambda (L)
+    (if (null? L)
+	'()
+	(append (renverse (cdr L)) (list (car L)))
+	)))
+
+(trace renverse)
+(renverse '(1 (2 (3 4)) 5))
+
+;; Miroir :
+;; Ex : (1 (2 (3 4)) 5) -> (5 ((4 3) 2) 1)
+;; () -> ()
+;; (e|L') -> (miroir L'|miroir e) si e est une liste
+;;        -> (miroir L'| e) sinon
+
+(define miroir
+  (lambda (L)
+    (if (null? L)
+	'()
+	(if (list? (car L))
+	    (append (miroir (cdr L)) (list (car L)))
+	    ))))
+
+(miroir '(1 (2 (3 4)) 5))
